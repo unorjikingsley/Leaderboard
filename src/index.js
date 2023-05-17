d5471191f82c3868684652957feefb1056f100cc
unorji kingsley <unorjikingsley@gmail.com>
2023-05-17 11:35:03 +0100

    Stored and get data from api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './index.css';
 import displayScores from './modules/table.js';
 import Score from './modules/score.js';
+import getScores from './modules/getscores.js';
+import addScore from './modules/apipost.js';
 
 const username = document.querySelector('#username');
 const userscore = document.querySelector('#userscore');
 const submitBtn = document.querySelector('#submit');
+const btn = document.querySelector('#btn');
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -13,8 +16,13 @@ submitBtn.addEventListener('click', (e) => {
     const currentname = username.value;
     const currentscore = userscore.value;
     const score = new Score(currentname, currentscore);
+    addScore(score);
     displayScores(score);
     username.value = '';
     userscore.value = '';
   }
+
+  btn.addEventListener('click', () => {
+    getScores();
+  });
 });
